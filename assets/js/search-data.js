97f13e61823c5a37c6462708f04f1a3a2bf0f01a
valermas <78051589+valermas@users.noreply.github.com>
2025-03-23 21:58:59 +0000

    Deploying to gh-pages from @ valermas/valermas.github.io@f56c1f7e5ce55a469728c17dd5ada0edf5bbd910 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -82,9 +82,9 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-steamed-happiness",
+          title: 'steamed happiness',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
