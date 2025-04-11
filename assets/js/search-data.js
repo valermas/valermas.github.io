5cc62e0b09bb38ff941d32897767cde5852ba094
valermas <78051589+valermas@users.noreply.github.com>
2025-04-11 14:16:51 +0000

    Deploying to gh-pages from @ valermas/valermas.github.io@45d7a0150095d9cdaf01edc14ca3ae47a93e54f6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -92,9 +92,9 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+            },},{id: "projects-spring-and-greek-pita",
+          title: 'spring and greek pita',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{id: "projects-project-6",
